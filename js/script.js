d934b40a1bb46e4b7bdcce786225f2b12ea01b94
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-23 09:04:13 +0530

    Added random password generation feature

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-document
-    .getElementById("generateBtn")
-    .addEventListener("click", function() {
+const passwordInput = document.getElementById("password");
+const generateBtn = document.getElementById("generateBtn");
 
-        alert("Day 2 me password generate karenge 🚀");
+generateBtn.addEventListener("click", function() {
 
-    });
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+    let password = "";
+
+    for (let i = 0; i < 12; i++) {
+
+        const randomIndex =
+            Math.floor(Math.random() * chars.length);
+
+        password += chars[randomIndex];
+    }
+
+    passwordInput.value = password;
+
+});
